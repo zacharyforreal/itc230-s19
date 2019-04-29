8674d0a1fa2d38c5cd6ea451e3199aacbc55f0b3
XingLi <zacharyforreal@gmail.com>
2019-04-29 21:35:31 +0000

    A3

--- assignment3/index.js
+++ assignment3/index.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
 
-const qs = require("querystring");
+const path = require('path');
 const http = require("http"), fs = require('fs');
 const movies = require('./lib/movies.js');
 
@@ -9,81 +9,71 @@ const express = require("express");
 const app = express();
 
 
-http.createServer((req,res) => {
-  let url = req.url.split("?");
-  let movie = qs.parse(url[1]);
-  let path = url[0];
-  
-  //https://itc230-s19-xingli313.c9users.io/get?title=workPlease
+const handlebars = require('express-handlebars')
+  .create({ defaultLayout: 'main' });
   
 
-  switch(path) {
-    
-    case '/':
-    fs.readFile("public/home.html", (err, data) => {
-     if (err) return console.error(err);
-     res.writeHead(200, {'Content-Type': 'text/html'});
-     res.end(data.toString());
-  });
-      break;
-      
-    case '/about':
-    fs.readFile("public/about.html", (err, data) => {
-     if (err) return console.error(err);
-     res.writeHead(200, {'Content-Type': 'text/html'});
-     res.end(data.toString());
-  });
-      break;
-      
-     //get specific movie 
-     case '/get':
-       //console.log(movie);
-      var input = movies.get(movie.title);
-      //console.log(input);
-      //if (input == NOT_FOUND) input = 'Not Found';
-      let results = (input) ? JSON.stringify(input) : "Not found";
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('results for ' + movie.title + '...............' + results);
-      break;
-      
-      
-    // get all movies
-    case '/getall':  
-      console.log('getall')
-      var allItems = movies.getAll();
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end(JSON.stringify(allItems));
-      break;
-      
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('body-parser')());
 
-    //delete one movie
-    case '/delete':
-      console.log('Deleted action')
-      var deleteItem = movies.delete(movie.title);
-      //console.log("A1")
-      let result = deleteItem;
-      if (result = (deleteItem > -1)) {
-      // let result = (deleteItem > -1) ? JSON.stringify(deleteItem) : "Not found"
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end("The movie " + movie.title + " has been deleted."); 
-      } else {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end("We can not find this movie. ");  
-      }
-      break;
-      
-    //add movie to the array 
-    case '/add':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end("\n \n"); // delete specific movie title
-      break;
 
-    
-      
-      
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found.  ');
-  }
-  
-}).listen(process.env.PORT || 3000);
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+app.get('/', function (req, res) {
+    res.type('text/html');
+    res.sendFile(__dirname + '/public/home.html');
+});
+
+
+
+app.get('/about', function (req, res) {
+    res.render('about', {
+        Title: movieCollections.title,
+        Releasedate: movieCollections.releasedate,
+        IMDBRating: movieCollections.IMDBRating
+    });
+});
+
+//get object
+app.get('/get', function (req, res) {
+    var item = movies.get(req.query.title)
+    if (item) {
+        res.send("Looking for movie called " + req.query.title + "\n" + JSON.stringify(item));
+    } else {
+        res.send("Movie not found");
+    }
+
+});
+
+app.post('/get', function (req, res) {
+    var item = movies.get(req.body.title)
+    res.render('details', { title: req.body.title, item: item });
+
+});
+
+//delete object
+app.get('/delete', function (req, res) {
+    var result = movies.delete(req.query.title)
+    console.log(result)
+    //res.send("Deleted Movie " + req.query.title + "\n" + JSON.stringify(result));
+    res.render('delete', { title: req.query.title, result: result });
+});
+
+
+// 404 handler
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+// 500 handler
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});  
